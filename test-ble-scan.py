87f794f73da99a0c4960b52b2fcbb556ecbd2cfd
Arifur4321 <arifur.rahman07@outlook.com>
2023-04-27 16:22:36 +0200

    arifur

--- test-ble-scan.py
+++ test-ble-scan.py
@@ -1,11 +1,9 @@
-from bluepy.btle import Scanner, DefaultDelegate
-import struct
+from bluepy.btle import Scanner, DefaultDelegate, UUID
 
-# Define a custom delegate class to handle Bluetooth device events
+# The service UUID used by Aerobits IDME Pro
+SERVICE_UUID = UUID("0000febb-0000-1000-8000-00805f9b34fb")
+
 class ScanDelegate(DefaultDelegate):
-    def __init__(self):
-        DefaultDelegate.__init__(self)
-
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if isNewDev:
             print("Discovered device:", dev.addr)
@@ -14,55 +12,40 @@ class ScanDelegate(DefaultDelegate):
         elif isNewData:
             print("Received new data from device:", dev.addr)
             for (adtype, desc, value) in dev.getScanData():
-                if desc == "Complete 16b Services":
-                    # Decode the 16-bit service data as an integer
-                    service_data = int(value, 16)
-                    print("    Service data (hex):", value)
-                    print("    Service data (int):", service_data)
-                    # Extract the Remote ID message type and version from the service data
-                    message_type = (service_data & 0b1111000000000000) >> 12
-                    version = (service_data & 0b0000111100000000) >> 8
-                    print("    Message type:", message_type)
-                    print("    Version:", version)
-                    # Extract the message data from the service data
-                    message_data = (service_data & 0b0000000011111111)
-                    if message_type == 1:
-                        print("    UAS ID:", message_data)
-                    elif message_type == 2:
-                        print("    Location (latitude):", struct.unpack('>i', struct.pack('>I', message_data << 8))[0] / 10000000.0)
-                    elif message_type == 3:
-                        print("    Location (longitude):", struct.unpack('>i', struct.pack('>I', message_data << 8))[0] / 10000000.0)
-                    elif message_type == 4:
-                        print("    Altitude (MSL):", message_data)
-                    elif message_type == 5:
-                        print("    Horizontal accuracy:", message_data)
-                    elif message_type == 6:
-                        print("    Vertical accuracy:", message_data)
-                    elif message_type == 7:
-                        print("    Barometric pressure:", message_data)
-                    elif message_type == 8:
-                        print("    Speed:", message_data)
-                    elif message_type == 9:
-                        print("    Course:", message_data)
-                    elif message_type == 10:
-                        print("    Timestamp:", message_data)
-                    elif message_type == 11:
-                        print("    Emergency status:", message_data)
+                if adtype == 22:  # 16-bit service data
+                    service_data = decode_service_data(value)
+                    if service_data:
+                        print("  Aerobits IDME Pro data:")
+                        for key, val in service_data.items():
+                            print(f"    {key}: {val}")
 
-# Initialize the Bluetooth scanner and delegate
-scanner = Scanner().withDelegate(ScanDelegate())
+def decode_service_data(data):
+    """
+    Decode the 16-bit service data broadcast by Aerobits IDME Pro.
 
-# Scan for Aerobits IDME Pro devices and print out their advertising data
-print("Scanning for Aerobits IDME Pro devices...")
-devices = scanner.scan(10.0)
-for dev in devices:
-    for (adtype, desc, value) in dev.getScanData():
-        if desc == "Complete Local Name" and "IDME" in value:
-            print("Aerobits IDME Pro found:")
-            print("  Device address:", dev.addr)
-            print("  Device name:", dev.getValueText(9))
-            print("  RSSI:", dev.rssi)
-            for (adtype, desc, value) in dev.getScanData():
-                if desc == "Complete 16b Services":
-                    # Decode the 16-bit service data as an integer
-                    service_data = int(value, 
+    :param data: The service data as a byte string.
+    :return: A dictionary of decoded values.
+    """
+    decoded = {}
+
+    # Check that the data starts with the service UUID
+    if data.startswith(SERVICE_UUID.bin()):
+        # Parse the values from the data
+        version = data[2]
+        flags = data[3]
+        latitude = int.from_bytes(data[4:8], byteorder="little", signed=True) / 1e7
+        longitude = int.from_bytes(data[8:12], byteorder="little", signed=True) / 1e7
+        altitude = int.from_bytes(data[12:14], byteorder="little", signed=True)
+        accuracy = data[14]
+        num_satellites = data[15]
+
+        # Store the values in the dictionary
+        decoded["version"] = version
+        decoded["flags"] = flags
+        decoded["latitude"] = latitude
+        decoded["longitude"] = longitude
+        decoded["altitude"] = altitude
+        decoded["accuracy"] = accuracy
+        decoded["num_satellites"] = num_satellites
+
+    return decoded
