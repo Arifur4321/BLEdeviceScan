de495e6daee3336b60a05a47714ecc26a2a8fc2a
a_rahman <a.rahman@nexum.srl>
2023-05-01 16:43:56 +0200

    arifur

--- test2-ble-scan.py
+++ test2-ble-scan.py
@@ -52,6 +52,24 @@ for dev in devices:
     print("  Advertising data:")
     for (adtype, desc, value) in dev.getScanData():
         print("    %s: %s" % (desc, value))
+
+        service_data = None
+        for (adtype, desc, value) in dev.getScanData():
+            if adtype == 22 and desc.startswith('16b Service Data'):
+                service_data = value
+                break
+
+            if service_data is not None:
+                # Parse the Service Data
+                uuid = binascii.unhexlify(service_data[0:4])[::-1]  # reverse byte order
+                data = binascii.unhexlify(service_data[4:])
+
+                # Print the parsed data
+                print("Service UUID:", binascii.hexlify(uuid))
+                print("Data:", binascii.hexlify(data))
+
+
+
         if adtype == 255:  # Manufacturer Specific Data
             if value.startswith(b'\x1d\xa5\x94\x01'):  # Aerobits IDME Pro
                 data = msgpack.unpackb(value[4:])
