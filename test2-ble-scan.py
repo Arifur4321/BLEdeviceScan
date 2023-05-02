2bc8929eb224011468debfa2e464778ca688e4ba
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-02 14:18:30 +0200

    arifur

--- test2-ble-scan.py
+++ test2-ble-scan.py
@@ -1,22 +1,40 @@
-from bluepy.btle import Peripheral, DefaultDelegate
+from bluepy.btle import Scanner, DefaultDelegate, UUID
 
-class NotificationDelegate(DefaultDelegate):
+# Define a custom delegate class to handle Bluetooth device events
+class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
 
-    def handleNotification(self, cHandle, data):
-        print("Received data: ", data.decode("utf-8"))
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev:
+            print("Discovered device:", dev.addr)
+            print("  Device name:", dev.getValueText(9))
+            print("  RSSI:", dev.rssi)
+        elif isNewData:
+            print("Received new data from device:", dev.addr)
 
-device_address = "7C:DF:A1:5A:4B:1E" # replace with the MAC address of your IDME Pro device
-device = Peripheral(device_address)
+        # Print out advertising data, including decoded 16-bit service data
+        print("  Advertising data:")
+        for (adtype, desc, value) in dev.getScanData():
+            print("    %s: %s" % (desc, value))
+            if adtype == 22:  # 16-bit service data
+                uuid = UUID(value[0:4], True)
+                data = value[4:]
+                print("    Decoded 16-bit service data:")
+                if uuid == UUID("1809"):  # Health Thermometer service
+                    temp = int.from_bytes(data, byteorder="little", signed=True) / 100
+                    print("      Temperature:", temp, "°C")
+                elif uuid == UUID("180D"):  # Heart Rate service
+                    bpm = int.from_bytes(data, byteorder="little")
+                    print("      Heart rate:", bpm, "bpm")
 
-advertising_data_uuid = "0000fff4-0000-1000-8000-00805f9b34fb" # replace with the UUID of the advertising data characteristic
-advertising_data_characteristic = device.getCharacteristics(uuid=advertising_data_uuid)[0]
-advertising_data_handle = advertising_data_characteristic.getHandle()
-device.writeCharacteristic(advertising_data_handle + 1, b"\x01\x00")
+# Initialize the Bluetooth scanner and delegate
+scanner = Scanner().withDelegate(ScanDelegate())
 
-device.withDelegate(NotificationDelegate())
-
+# Scan for Bluetooth devices indefinitely and print out their advertising data
 while True:
-    if device.waitForNotifications(1.0):
-        continue
+    devices = scanner.scan(2.0)
+    for dev in devices:
+        print("Device address:", dev.addr)
+        print("  Device name:", dev.getValueText(9))
+        print("  RSSI:", dev.rssi)
