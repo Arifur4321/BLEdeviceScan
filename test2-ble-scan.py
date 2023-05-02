0a701e42d60d5442baca185801001f05a57f6ee0
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-02 13:08:02 +0200

    arifur

--- test2-ble-scan.py
+++ test2-ble-scan.py
@@ -1,6 +1,10 @@
 import msgpack
 from bluepy.btle import Scanner, DefaultDelegate, UUID
 import binascii
+
+# Define the UUID of the Aerobits IDME Pro service
+AEROBITS_SERVICE_UUID = UUID("58da3605-5d5e-11e9-8647-d663bd873d93")
+
 # Define a custom delegate class to handle Bluetooth device events
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
@@ -12,61 +16,29 @@ class ScanDelegate(DefaultDelegate):
             print("  Device name:", dev.getValueText(9))
             print("  RSSI:", dev.rssi)
 
-             
-
         elif isNewData:
             print("Received new data from device:", dev.addr)
-            
-             # Parse Aerobits IDME Pro advertising data
-            # Parse Aerobits IDME Pro advertising data
-        ad = dev.getScanData()
-        for (adtype, desc, value) in ad:
-            if adtype == 22 and value.startswith(b'\xfa\xff\x0d\x1c'.decode()):
-                altitude = int.from_bytes(value[7:9], byteorder='big', signed=True) * 0.1
-                latitude = int.from_bytes(value[9:13], byteorder='big', signed=True) * 0.0001
-                longitude = int.from_bytes(value[13:17], byteorder='big', signed=True) * 0.0001
-                distance = int.from_bytes(value[17:19], byteorder='big', signed=True) * 0.1
-                print("Decoded data:")
-                print("  Altitude:", altitude)
-                print("  Latitude:", latitude)
-                print("  Longitude:", longitude)
-                print("  Distance:", distance)
 
-
-
+            # Check if the service data belongs to the Aerobits IDME Pro device
             service_data = None
             for (adtype, desc, value) in dev.getScanData():
                 if adtype == 22 and desc.startswith('16b Service Data'):
                     service_data = value
                     break
-
             if service_data is not None:
-                # Parse the Service Data
                 uuid = binascii.unhexlify(service_data[0:4])[::-1]  # reverse byte order
-                data = binascii.unhexlify(service_data[4:])
+                if uuid == bytes(AEROBITS_SERVICE_UUID):
+                    # Parse the Aerobits IDME Pro advertising data
+                    altitude = int.from_bytes(service_data[5:7], byteorder='big', signed=True) * 0.1
+                    latitude = int.from_bytes(service_data[7:11], byteorder='big', signed=True) * 0.0001
+                    longitude = int.from_bytes(service_data[11:15], byteorder='big', signed=True) * 0.0001
+                    distance = int.from_bytes(service_data[15:17], byteorder='big', signed=True) * 0.1
+                    print("Decoded data:")
+                    print("  Altitude:", altitude)
+                    print("  Latitude:", latitude)
+                    print("  Longitude:", longitude)
+                    print("  Distance:", distance)
 
-                # Print the parsed data
-                print("Service UUID:", binascii.hexlify(uuid))
-                print("Data:", binascii.hexlify(data))
-
-            # Decode MessagePack data
-            for (adtype, desc, value) in dev.getScanData():
-                if adtype == 255:  # Manufacturer Specific Data
-                    if value.startswith(b'\x1d\xa5\x94\x01'):  # Aerobits IDME Pro
-                        data = msgpack.unpackb(value[4:])
-                        print("Decoded data:", data)
-                        print("Altitude:", data['altitude'])
-                        print("Latitude:", data['latitude'])
-                        print("Longitude:", data['longitude'])
-                        print("Distance:", data['distance'])
-            
-            service_data = dev.getValueText(22)
-            
-            if service_data is not None and service_data.startswith('16b'):
-                # Extract the raw bytes from the service data string
-                raw_data = binascii.unhexlify(service_data[6:])
-
-             
 # Initialize the Bluetooth scanner and delegate
 scanner = Scanner().withDelegate(ScanDelegate())
 
@@ -79,21 +51,3 @@ for dev in devices:
     print("  Advertising data:")
     for (adtype, desc, value) in dev.getScanData():
         print("    %s: %s" % (desc, value))
-
-        if desc.startswith('16b'):
-
-            print ("  raw decode data > ",  value)
-             
-            # Parse Aerobits IDME Pro advertising data
-    ad = dev.getScanData()
-    for (adtype, desc, value) in ad:
-        if adtype == 22 and value.startswith(b'\xfa\xff\x0d\x1c'.decode()):
-            altitude = int.from_bytes(value[7:9], byteorder='big', signed=True) * 0.1
-            latitude = int.from_bytes(value[9:13], byteorder='big', signed=True) * 0.0001
-            longitude = int.from_bytes(value[13:17], byteorder='big', signed=True) * 0.0001
-            distance = int.from_bytes(value[17:19], byteorder='big', signed=True) * 0.1
-            print("Decoded data:")
-            print("  Altitude:", altitude)
-            print("  Latitude:", latitude)
-            print("  Longitude:", longitude)
-            print("  Distance:", distance)
