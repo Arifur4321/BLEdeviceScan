82e37937300d0f07c9a2b3c975e034bd95c4f9e4
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-02 16:08:53 +0200

    arifur

--- test2-ble-scan.py
+++ test2-ble-scan.py
@@ -12,6 +12,49 @@ class ScanDelegate(DefaultDelegate):
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if isNewDev:
             print(f"Discovered device {dev.addr}")
+                        # Check if the device is an Aerobits IDME Pro device
+            value_text = dev.getValueText(9)
+            print("dev.getValueText(9) :", value_text)
+            if value_text is not None and "IDME" in value_text:
+                # Connect to the device
+                print(f"Connecting to IDME Pro device {dev.addr}")
+                idme_device = dev.connect()
+
+                # Get the GPS data
+                try:
+                    gps_data = gpsd.get_current()
+                    if gps_data.mode >= 2:
+                        latitude = gps_data.lat
+                        longitude = gps_data.lon
+                        print(f"IDME Pro device {dev.addr} location: ({latitude}, {longitude})")
+                    else:
+                        print(f"IDME Pro device {dev.addr} location: Unknown")
+                except Exception as e:
+                    print(f"Error getting GPS data for IDME Pro device {dev.addr}: {e}")
+
+                # Disconnect from the device
+                idme_device.disconnect()
+
+            # Check if the device is a GPS device
+            elif "GPS" in value_text:
+                # Connect to the device
+                print(f"Connecting to GPS device {dev.addr}")
+                gps_device = dev.connect()
+
+                # Get the GPS data
+                try:
+                    gps_data = gpsd.get_current()
+                    if gps_data.mode >= 2:
+                        latitude = gps_data.lat
+                        longitude = gps_data.lon
+                        print(f"GPS device {dev.addr} location: ({latitude}, {longitude})")
+                    else:
+                        print(f"GPS device {dev.addr} location: Unknown")
+                except Exception as e:
+                    print(f"Error getting GPS data for GPS device {dev.addr}: {e}")
+
+                # Disconnect from the device
+                gps_device.disconnect()
         elif isNewData:
             print(f"Received new data from device {dev.addr}")
 
