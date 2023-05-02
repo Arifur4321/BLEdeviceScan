8cf6d24d906f480ab501b7745ba945b45d39f883
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-02 15:33:53 +0200

    arifur

--- test2-ble-scan.py
+++ test2-ble-scan.py
@@ -1,40 +1,37 @@
-from bluepy.btle import Scanner, DefaultDelegate
-from geopy.geocoders import Nominatim
+import gps
+from bluetooth import discover_devices, BluetoothError
 
-# Define a custom delegate class to handle Bluetooth device events
-class ScanDelegate(DefaultDelegate):
-    def __init__(self):
-        DefaultDelegate.__init__(self)
+# Connect to the GPS module
+session = gps.gps("localhost", "2947")
+session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-    def handleDiscovery(self, dev, isNewDev, isNewData):
-        if isNewDev:
-            print("Discovered device:", dev.addr)
-            print("  Device name:", dev.getValueText(9))
-            print("  RSSI:", dev.rssi)
-            # Get the location of the device using GPS
-            geolocator = Nominatim(user_agent="my_app")
-            location = geolocator.reverse(f"{dev.addr}")
-            print("  Latitude:", location.latitude)
-            print("  Longitude:", location.longitude)
-            print("  Altitude:", location.altitude)
-            print("  Height:", location.raw['address']['floor'])
+# Define a function to get the GPS data
+def get_gps_data():
+    try:
+        report = session.next(timeout=1)
+        if report['class'] == 'TPV':
+            if hasattr(report, 'lat') and hasattr(report, 'lon'):
+                latitude = report.lat
+                longitude = report.lon
+                return (latitude, longitude)
+    except Exception as e:
+        print(e)
+    return None
 
-# Initialize the Bluetooth scanner and delegate
-scanner = Scanner().withDelegate(ScanDelegate())
+# Discover nearby Bluetooth devices that support SPP
+devices = discover_devices(lookup_names=True, lookup_class=True)
+spp_devices = []
+for device, name, device_class in devices:
+    if device_class[0] == 0x01 and device_class[1] == 0x00 and device_class[2] == 0x00:
+        spp_devices.append((device, name))
 
-# Scan for Bluetooth devices and print out their advertising data and location
-devices = scanner.scan(10.0)
-for dev in devices:
-    print("Device address:", dev.addr)
-    print("  Device name:", dev.getValueText(9))
-    print("  RSSI:", dev.rssi)
-    print("  Advertising data:")
-    for (adtype, desc, value) in dev.getScanData():
-        print("    %s: %s" % (desc, value))
-    # Get the location of the device using GPS
-    geolocator = Nominatim(user_agent="my_app")
-    location = geolocator.reverse(f"{dev.addr}")
-    print("  Latitude:", location.latitude)
-    print("  Longitude:", location.longitude)
-    print("  Altitude:", location.altitude)
-    print("  Height:", location.raw['address']['floor'])
+# Loop through the devices and get their location
+for device, name in spp_devices:
+    # Get the latitude and longitude of the device
+    location = get_gps_data()
+
+    # Print the device name and location
+    if location is not None:
+        print(f"Device: {name}, Location: ({location[0]}, {location[1]})")
+    else:
+        print(f"Device: {name}, Location: Unknown")
