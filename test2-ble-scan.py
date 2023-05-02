d99902970c718a690cdda5d41d951e5506e210bc
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-02 14:45:35 +0200

    arifur

--- test2-ble-scan.py
+++ test2-ble-scan.py
@@ -17,14 +17,9 @@ class ScanDelegate(DefaultDelegate):
         print("  Advertising data:")
         for (adtype, desc, value) in dev.getScanData():
             print("    %s: %s" % (desc, value))
-            data = value[4:]
-            uuid = UUID(value[0:4], True)
-            print("data is :",  data)
-            print("uuid  is :",  uuid)
             if adtype == 22:  # 16-bit service data
                 uuid = UUID(value[0:4], True)
                 data = value[4:]
-                print("data is :",  data)
                 print("    Decoded 16-bit service data:")
                 if uuid == UUID("0000ffe0-0000-1000-8000-00805f9b34fb"):  # Aerobits IDME Pro service
                     if len(data) == 4:
@@ -42,10 +37,29 @@ class ScanDelegate(DefaultDelegate):
 # Initialize the Bluetooth scanner and delegate
 scanner = Scanner().withDelegate(ScanDelegate())
 
-# Scan for Bluetooth devices indefinitely and print out their advertising data
+# Continuously scan for the Aerobits IDME Pro device and print out its advertising data when detected
 while True:
     devices = scanner.scan(2.0)
     for dev in devices:
-        print("Device address:", dev.addr)
-        print("  Device name:", dev.getValueText(9))
-        print("  RSSI:", dev.rssi)
+        if dev.addr == "AEROBITS_IDME_PRO_DEVICE_ADDRESS":
+            print("Device address:", dev.addr)
+            print("  Device name:", dev.getValueText(9))
+            print("  RSSI:", dev.rssi)
+            for (adtype, desc, value) in dev.getScanData():
+                print("  %s: %s" % (desc, value))
+            print("  Decoded 16-bit service data:")
+            for (adtype, desc, value) in dev.getScanData(22):
+                uuid = UUID(value[0:4], True)
+                data = value[4:]
+                if uuid == UUID("0000ffe0-0000-1000-8000-00805f9b34fb"):  # Aerobits IDME Pro service
+                    if len(data) == 4:
+                        # Decode the data as follows:
+                        # Byte 0: Battery level (0-100%)
+                        # Byte 1: Temperature (in degrees Celsius)
+                        # Byte 2-3: Humidity (in %RH, little-endian)
+                        battery_level = data[0]
+                        temperature = data[1]
+                        humidity = int.from_bytes(data[2:4], byteorder="little")
+                        print("    Battery level:", battery_level, "%")
+                        print("    Temperature:", temperature, "°C")
+                        print("    Humidity:", humidity, "%RH")
