f625a433c3dea0d343c63a4c9afc60c62637df99
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-04 14:43:01 +0200

    arifur

--- test3-scan.py
+++ test3-scan.py
@@ -1,4 +1,5 @@
 from bluepy.btle import Scanner, DefaultDelegate
+import math
 
 # Define a custom delegate class to handle Bluetooth device events
 class ScanDelegate(DefaultDelegate):
@@ -10,58 +11,28 @@ class ScanDelegate(DefaultDelegate):
             print("Discovered device:", dev.addr)
             print("  Device name:", dev.getValueText(9))
             print("  RSSI:", dev.rssi)
+            # Estimate the distance based on the RSSI value
+            distance = self.estimateDistance(dev.rssi)
+            print("  Estimated distance:", distance, "meters")
         elif isNewData:
             print("Received new data from device:", dev.addr)
-            # Parse the advertising data according to the ASD-STAN standard
-            data = dev.getValueText(255)
-            print(" ASD-STAN standard data",data)
-            if data is not None:
-                # Extract the data elements from the advertising data
-                elements = [data[i:i+2] for i in range(0, len(data), 2)]
-                i = 0
-                while i < len(elements):
-                    element_id = int(elements[i], 16)
-                    element_len = int(elements[i+1], 16)
-                    element_data = elements[i+2:i+2+element_len]
-                    i += 2 + element_len
-                    # Decode the data element based on its ID
-                    if element_id == 0x01:
-                        print("  Flags:", element_data)
-                    elif element_id == 0x02:
-                        print("  Incomplete List of 16-bit Service Class UUIDs:", element_data)
-                    elif element_id == 0x03:
-                        print("  Complete List of 16-bit Service Class UUIDs:", element_data)
-                    elif element_id == 0x04:
-                        print("  Incomplete List of 32-bit Service Class UUIDs:", element_data)
-                    elif element_id == 0x05:
-                        print("  Complete List of 32-bit Service Class UUIDs:", element_data)
-                    elif element_id == 0x06:
-                        print("  Incomplete List of 128-bit Service Class UUIDs:", element_data)
-                    elif element_id == 0x07:
-                        print("  Complete List of 128-bit Service Class UUIDs:", element_data)
-                    elif element_id == 0x08:
-                        print("  Shortened Local Name:", element_data)
-                    elif element_id == 0x09:
-                        print("  Complete Local Name:", element_data)
-                    elif element_id == 0x0a:
-                        print("  TX Power Level:", element_data)
-                    elif element_id == 0x0d:
-                        print("  Class of Device:", element_data)
-                    elif element_id == 0x16:
-                        print("  Service Data:", element_data)
-                    elif element_id == 0xff:
-                        print("  Manufacturer Specific Data:", element_data)
+
+    def estimateDistance(self, rssi):
+        # Calculate the distance based on the RSSI value using the log-distance path loss model
+        # The constants used in this formula are based on empirical measurements and can vary depending on the environment
+        txPower = -59 # The transmit power of the BLE device in dBm
+        n = 2.0 # The path loss exponent, which depends on the environment (e.g. free space, indoors, etc.)
+        return math.pow(10, (txPower - rssi) / (10 * n))
 
 # Initialize the Bluetooth scanner and delegate
 scanner = Scanner().withDelegate(ScanDelegate())
 
-# Scan for Bluetooth devices and print out their advertising data
+# Scan for Bluetooth devices and print out their device address, name, RSSI, and estimated distance
 devices = scanner.scan(10.0)
 for dev in devices:
     print("Device address:", dev.addr)
     print("  Device name:", dev.getValueText(9))
     print("  RSSI:", dev.rssi)
-    print("  Advertising data:")
-    data = dev.getValueText(255)
-    if data is not None:
-        print("    Raw data:", data)
+    # Estimate the distance based on the RSSI value
+    distance = ScanDelegate().estimateDistance(dev.rssi)
+    print("  Estimated distance:", distance, "meters")
