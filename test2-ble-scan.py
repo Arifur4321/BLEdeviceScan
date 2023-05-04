ecb6a591372cacb9ff8b8448cecc8f60f7139c16
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-04 13:38:37 +0200

    arifur

--- test2-ble-scan.py
+++ test2-ble-scan.py
@@ -3,46 +3,60 @@ from bluepy.btle import Scanner, DefaultDelegate
 
 # Define a custom delegate class to handle Bluetooth device events
 class ScanDelegate(DefaultDelegate):
-    def __init__(self):
+    def __init__(self, wifi_data):
         DefaultDelegate.__init__(self)
+        self.wifi_data = wifi_data
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if isNewDev:
             print("Discovered device:", dev.addr)
             print("  Device name:", dev.getValueText(9))
             print("  RSSI:", dev.rssi)
-            location = get_location(dev.addr)
+            location = get_location(dev.addr, self.wifi_data)
             if location:
                 print("  Location: %f, %f" % (location['lat'], location['lng']))
         elif isNewData:
             print("Received new data from device:", dev.addr)
 
 # Initialize the Bluetooth scanner and delegate
-scanner = Scanner().withDelegate(ScanDelegate())
+scanner = Scanner().withDelegate(ScanDelegate([]))
 
 # Define the Google Maps Geolocation API endpoint and API key
 endpoint = "https://www.googleapis.com/geolocation/v1/geolocate"
 api_key = "AIzaSyBqyGDMUcp4FZPGL6XICmX9ImxYzpIH99M"
 
-# Define a function to get the location of a Wi-Fi access point using the Google Maps Geolocation API
-def get_location(mac_address):
-    # Scan for nearby Wi-Fi access points and collect their MAC addresses and signal strengths
-    wifi_data = []
+# Define a function to scan for nearby Wi-Fi access points and collect their MAC addresses and signal strengths
+def scan_wifi():
+    scanner = Scanner()
     devices = scanner.scan(10.0)
+    wifi_data = []
     for dev in devices:
         for (adtype, desc, value) in dev.getScanData():
-            if desc == "Complete Local Name" and value == mac_address:
+            if desc == "Complete Local Name":
                 wifi_data.append({"macAddress": dev.addr, "signalStrength": dev.rssi})
+    return wifi_data
 
-    # If at least one Wi-Fi access point was found, send a request to the Google Maps Geolocation API to get the location
-    if wifi_data:
-        payload = {"wifiAccessPoints": wifi_data}
-        headers = {"Content-Type": "application/json", "Authorization": "key=" + api_key}
-        response = requests.post(endpoint, json=payload, headers=headers)
-        if response.status_code == 200:
-            location = response.json()['location']
-            accuracy = response.json()['accuracy']
-            return {"lat": location['lat'], "lng": location['lng'], "accuracy": accuracy}
+# Define a function to get the location of a device using the Google Maps Geolocation API and nearby Wi-Fi access points
+def get_location(mac_address, wifi_data):
+    # If there are no nearby Wi-Fi access points, scan for them and update the global wifi_data variable
+    if not wifi_data:
+        wifi_data = scan_wifi()
+
+    # Look up the MAC address in the list of nearby Wi-Fi access points and send a request to the Google Maps Geolocation API to get the location
+    for wifi in wifi_data:
+        if wifi['macAddress'] == mac_address:
+            payload = {"wifiAccessPoints": [wifi]}
+            headers = {"Content-Type": "application/json", "Authorization": "key=" + api_key}
+            response = requests.post(endpoint, json=payload, headers=headers)
+            if response.status_code == 200:
+                location = response.json()['location']
+                return {"lat": location['lat'], "lng": location['lng']}
+
+# Scan for nearby Wi-Fi access points and store the results in the wifi_data variable
+wifi_data = scan_wifi()
+
+# Initialize the Bluetooth scanner and delegate with the wifi_data variable
+scanner = Scanner().withDelegate(ScanDelegate(wifi_data))
 
 # Scan for Bluetooth devices and print out their information and location data
 devices = scanner.scan(10.0)
@@ -50,6 +64,6 @@ for dev in devices:
     print("Device address:", dev.addr)
     print("  Device name:", dev.getValueText(9))
     print("  RSSI:", dev.rssi)
-    location = get_location(dev.addr)
+    location = get_location(dev.addr, wifi_data)
     if location:
-        print("  Location: %f, %f (accuracy: %f meters)" % (location['lat'], location['lng'], location['accuracy']))
+        print("  Location: %f, %f" % (location['lat'], location['lng'])))
