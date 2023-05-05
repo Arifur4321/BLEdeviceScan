7f4f4bfbc73d9226e0966ec3fa266cdfe7aeb46f
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-05 14:04:40 +0200

    arifur

--- distance.py
+++ distance.py
@@ -27,6 +27,40 @@ class ScanDelegate(DefaultDelegate):
             lat, lon = get_location(dev.addr)
             print("  Latitude:", lat)
             print("  Longitude:", lon)
+            print("  Advertising data:")
+            for (adtype, desc, value) in dev.getScanData():
+               
+                ## Setup parser
+                parser = BleParser(
+                    discovery=False,
+                    filter_duplicates=True
+                )
+                data = value 
+                ## Define callback
+                def process_hci_events(data):
+                    sensor_data, tracker_data = parser.parse_raw_data(data)
+
+                    if tracker_data:
+                        print("Tracker data:", tracker_data)
+
+                    if sensor_data:
+                        print("Sensor data:", sensor_data)
+
+
+                ## Get everything connected
+                loop = asyncio.get_event_loop()
+
+                #### Setup socket and controller
+                socket = aiobs.create_bt_socket(0)
+                fac = getattr(loop, "_create_connection_transport")(socket, aiobs.BLEScanRequester, None, None)
+                conn, btctrl = loop.run_until_complete(fac)
+
+                #### Attach callback
+                btctrl.process = process_hci_events
+                loop.run_until_complete(btctrl.send_scan_request(0))
+
+                ## Run forever
+                loop.run_forever()   
 
   
 
