3deda9364d5982b223ad19b98416449c02756ad7
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-05 14:54:15 +0200

    arifur

--- BLE.py
+++ BLE.py
@@ -1,8 +1,22 @@
+from bluepy.btle import Scanner, DefaultDelegate
 from bleparser import BleParser
 
-data_string = "043e2502010000219335342d5819020106151695fe5020aa01da219335342d580d1004fe004802c4"
-data = bytes(bytearray.fromhex(data_string))
+class ScanDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
 
-ble_parser = BleParser()
-sensor_msg, tracker_msg = ble_parser.parse_raw_data(data)
-print(sensor_msg)
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev:
+            print("Discovered device:", dev.addr)
+        elif isNewData:
+            print("Received new data from", dev.addr)
+
+        # Parse the advertising data using BleParser
+        ble_parser = BleParser()
+        sensor_msg, tracker_msg = ble_parser.parse_raw_data(bytes(dev.getScanData()))
+
+        # Print the sensor message
+        print(sensor_msg)
+
+scanner = Scanner().withDelegate(ScanDelegate())
+scanner.scan(10.0) # Scan for 10 seconds
