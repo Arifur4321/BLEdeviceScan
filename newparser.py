9e7800cd1cad7267824d98bd060e80a96c8979c4
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-05 14:33:46 +0200

    arifur

--- newparser.py
+++ newparser.py
@@ -8,6 +8,7 @@ import requests
 from bluepy.btle import Scanner, DefaultDelegate, BTLEDisconnectError
 import aioblescan as aiobs
 from newparser import BleParser
+
 # Define a custom delegate class to handle Bluetooth device events
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
@@ -22,54 +23,26 @@ class ScanDelegate(DefaultDelegate):
             print("Received new data from device:", dev.addr)
             for (adtype, desc, value) in dev.getScanData():
                 print("    %s: %s" % (desc, value))
-                
-## Setup parser
-                parser = BleParser(
-                    discovery=False,
-                    filter_duplicates=True
-                )
+                if desc == "Manufacturer":
+                    parser.parse_manufacturer_data(value)
 
-                   ## Get everything connected
-                loop = asyncio.get_event_loop()
+# Setup parser
+parser = BleParser(
+    discovery=False,
+    filter_duplicates=True
+)
 
-                #### Setup socket and controller
-                socket = aiobs.create_bt_socket(0)
-                fac = getattr(loop, "_create_connection_transport")(socket, aiobs.BLEScanRequester, None, None)
-                conn, btctrl = loop.run_until_complete(fac)
+# Get everything connected
+loop = asyncio.get_event_loop()
 
-                #### Attach callback
-                btctrl.process = self.process_hci_events(value)
-                loop.run_until_complete(btctrl.send_scan_request(0))
+#### Setup socket and controller
+socket = aiobs.create_bt_socket(0)
+fac = getattr(loop, "_create_connection_transport")(socket, aiobs.BLEScanRequester, None, None)
+conn, btctrl = loop.run_until_complete(fac)
 
-                ## Run forever
-                loop.run_forever()
+#### Attach callback
+btctrl.process = parser.parse_raw_data
+loop.run_until_complete(btctrl.send_scan_request(0))
 
-        ## Define callback
-    def process_hci_events(data):
-        sensor_data, tracker_data = parser.parse_raw_data(data)
-
-        if tracker_data:
-            print("Tracker data:", tracker_data)
-
-        if sensor_data:
-            print("Sensor data:", sensor_data)
-
-
-  
-scanner = Scanner().withDelegate(ScanDelegate())
-
-# Scan for Bluetooth devices and print out their advertising data
-while True:
-    try:
-        devices = scanner.scan(10.0)
-        for dev in devices:
-            print("Device address:", dev.addr)
-            print("  Device name:", dev.getValueText(9))
-            print("  RSSI:", dev.rssi)
-            print("  Advertising data:")
-            for (adtype, desc, value) in dev.getScanData():
-                print("    %s: %s" % (desc, value))
-                
-    except BTLEDisconnectError:
-        print("Device disconnected")
-    time.sleep(10)  # Delay for 10 seconds before scanning again   
+## Run forever
+loop.run_forever()
