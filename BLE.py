42aff039c595ca911c329cb73dd1547881460847
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-05 15:01:08 +0200

    arifur

--- BLE.py
+++ BLE.py
@@ -13,10 +13,12 @@ class ScanDelegate(DefaultDelegate):
 
         # Parse the advertising data using BleParser
         ble_parser = BleParser()
-        advertising_data = dev.getScanData()
-        for data in advertising_data:
+        for (adtype, desc, value) in dev.getScanData():
+            print("    %s: %s" % (desc, value))
+            data_string = value
+            data = bytes(bytearray.fromhex(data_string))
             if data[0] == 255: # Check if the advertising data is manufacturer-specific
-                sensor_msg, tracker_msg = ble_parser.parse_raw_data(bytes(data[2]))
+                sensor_msg, tracker_msg = ble_parser.parse_raw_data(data)
                 print(sensor_msg)
 
 scanner = Scanner().withDelegate(ScanDelegate())
