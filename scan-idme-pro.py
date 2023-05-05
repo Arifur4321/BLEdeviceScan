58e36be4364d3de24e55955e3eaadb88cc4093f7
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-05 13:34:31 +0200

    arifur

--- scan-idme-pro.py
+++ scan-idme-pro.py
@@ -22,20 +22,31 @@ class ScanDelegate(DefaultDelegate):
             print("  RSSI:", dev.rssi)
         elif isNewData:
             print("Received new data from device:", dev.addr)
+            for (adtype, desc, value) in dev.getScanData():
+                print("    %s: %s" % (desc, value))
+                
+                ble_parser = BleParser()
+                sensor_msg, tracker_msg = ble_parser.parse_raw_data(value)
+                print(sensor_msg)
 
 # Initialize the Bluetooth scanner and delegate
 scanner = Scanner().withDelegate(ScanDelegate())
 
 # Scan for Bluetooth devices and print out their advertising data
-devices = scanner.scan(100.0)
-for dev in devices:
-    print("Device address:", dev.addr)
-    print("  Device name:", dev.getValueText(9))
-    print("  RSSI:", dev.rssi)
-    print("  Advertising data:")
-    for (adtype, desc, value) in dev.getScanData():
-        print("    %s: %s" % (desc, value))
-        
-        ble_parser = BleParser()
-        sensor_msg, tracker_msg = ble_parser.parse_raw_data(value)
-        print(sensor_msg)
+while True:
+    try:
+        devices = scanner.scan(10.0)
+        for dev in devices:
+            print("Device address:", dev.addr)
+            print("  Device name:", dev.getValueText(9))
+            print("  RSSI:", dev.rssi)
+            print("  Advertising data:")
+            for (adtype, desc, value) in dev.getScanData():
+                print("    %s: %s" % (desc, value))
+                
+                ble_parser = BleParser()
+                sensor_msg, tracker_msg = ble_parser.parse_raw_data(value)
+                print(sensor_msg)
+    except BTLEDisconnectError:
+        print("Device disconnected")
+    time.sleep(10)  # Delay for 10 seconds before scanning again   
