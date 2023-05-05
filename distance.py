8688cc194a57773e5f827760455739c5ddb34bc4
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-05 12:59:51 +0200

    arifur

--- distance.py
+++ distance.py
@@ -1,62 +1,135 @@
-import serial
+import requests
+import json
+from bluepy.btle import Scanner, DefaultDelegate, BTLEDisconnectError
+import struct
+import struct
+import asyncio
+import bleak
 import time
-your_com_port = "COM18"  # Change this to the com port your dongle is connected to.
-connecting_to_dongle = True
-print("Connecting to dongle...")
-# Trying to connect to dongle until connected. Make sure the port and baudrate is the same as your dongle.
-# You can check in the device manager to see what port then right-click and choose properties then the Port Settings
-# tab to see the other settings
-while connecting_to_dongle:
+
+from bleparser import BleParser
+
+# Define a custom delegate class to handle Bluetooth device events
+class ScanDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev:
+            print("Discovered device:", dev.addr)
+            print("  Device name:", dev.getValueText(9))
+            print("  RSSI:", dev.rssi)
+            # Estimate the distance based on the RSSI value
+            distance = ScanDelegate().estimateDistance(dev.rssi)
+            print("  Estimated distance:", distance, "meters")
+            lat, lon = get_location(dev.addr)
+            print("  Latitude:", lat)
+            print("  Longitude:", lon)
+
+            print("  Advertising data:")
+            for (adtype, desc, value) in dev.getScanData():
+                print("    %s: %s" % (desc, value))
+                data = bytes(bytearray.fromhex(value))
+
+                ble_parser = BleParser()
+                sensor_msg, tracker_msg = ble_parser.parse_raw_data(data)
+                print(sensor_msg)
+
+
+
+        elif isNewData:
+            print("Received new data from device:", dev.addr)
+
+            print("  Advertising data:")
+            for (adtype, desc, value) in dev.getScanData():
+                print("    %s: %s" % (desc, value))
+                data = bytes(bytearray.fromhex(value))
+
+                ble_parser = BleParser()
+                sensor_msg, tracker_msg = ble_parser.parse_raw_data(data)
+                print(sensor_msg)
+
+        
+    def estimateDistance(self, rssi):
+            # Calculate the distance based on the RSSI value using the log-distance path loss model
+            # The constants used in this formula are based on empirical measurements and can vary depending on the environment
+        txPower = -59 # The transmit power of the BLE device in dBm
+        n = 2.0 # The path loss exponent, which depends on the environment (e.g. free space, indoors, etc.)
+        return math.pow(10, (txPower - rssi) / (10 * n))
+
+# Google Maps Geolocation API endpoint and API key
+url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBqyGDMUcp4FZPGL6XICmX9ImxYzpIH99M"
+
+# Function to get the latitude and longitude of a device using the Google Maps Geolocation API
+def get_location(mac_address):
+    # Scan for Wi-Fi access points and get their MAC addresses and signal strengths
+    wifi_access_points = []
+    wifi_scanner = Scanner()
+    wifi_devices = wifi_scanner.scan(5.0)
+    for wifi_device in wifi_devices:
+        for (adtype, desc, value) in wifi_device.getScanData():
+            if desc == "Complete Local Name" and value == mac_address:
+                wifi_access_points.append({
+                    "macAddress": wifi_device.addr.replace(":", ""),
+                    "signalStrength": wifi_device.rssi
+                })
+
+    # Send a POST request to the Google Maps Geolocation API with the Wi-Fi access points and cell towers
+    data = {
+        "wifiAccessPoints": wifi_access_points
+    }
+    response = requests.post(url, data=json.dumps(data))
+
+    # Parse the response and return the latitude and longitude
+    if response.status_code == 200:
+        location = response.json()["location"]
+        return location["lat"], location["lng"]
+    else:
+        return None, None
+
+# Function to extract the 16-bit service data from the advertising data
+def get_service_data(dev):
+    service_data = {}
+    for (adtype, desc, value) in dev.getScanData():
+        if adtype == 22:  # 16-bit service data
+            uuid = value[:4]
+            data = value[4:]
+            service_data[uuid] = struct.unpack("<h", data)[0]
+    return service_data if service_data else None
+
+
+
+# Initialize the Bluetooth scanner and delegate
+scanner = Scanner().withDelegate(ScanDelegate())
+
+# Continuously scan for Bluetooth devices and print out their information
+while True:
     try:
-        console = serial.Serial(
-            port=your_com_port,
-            baudrate=57600,
-            parity="N",
-            stopbits=1,
-            bytesize=8,
-            timeout=0,
-        )
-        if console.is_open.__bool__():
-            connecting_to_dongle = False
-    except:
-        print("Dongle not connected. Please reconnect Dongle.")
-        time.sleep(5)
-print("Connected to Dongle.")
-# function to convert rssi to distance in meter
-def rssiToDistance(rssi):    
-  n=2
-  mp=-69
-  return round(10 ** ((mp - (int(rssi)))/(10 * n)),2)    
-#put the dongle in dual role, so we can scan for nearby device
-console.write(str.encode("AT+CENTRAL"))
-console.write("\r".encode())
-print("Putting dongle in Central role.")
-time.sleep(0.1)
-# Scan for nearby devices for 3 seconds
-console.write(str.encode("AT+GAPSCAN=3"))
-console.write("\r".encode())
-time.sleep(0.1)
-print("Looking for nearby Bluetooth devices ...")
-dongle_output2 = console.read(console.in_waiting)
-time.sleep(3)
-print("Scan Complete!")
-filtered = []
-# Filter out unncecssary outputs and keep only the list of devices (also remove index)
-for dev in dongle_output2.decode().splitlines():
-    if len(dev)>20:
-        filtered.append(dev.split(maxsplit=1)[1])
-# Get unique device by device id and add distance to each raw        
-seen = set()
-out = []
-for elem in filtered:
-    prefix = elem.split(' ')[1]
-    if prefix not in seen:
-        seen.add(prefix)
-        out.append(elem + " Distance: "+str(rssiToDistance(elem.split()[3]))+" meter") 
-# sort list by closest device
-out.sort(key=lambda x:int(x.split()[3]),reverse=True)
-# print(out)
-for i in range(0, len(out)):
-    print (out[i]) 
-time.sleep(0.1)
-console.close()
+        devices = scanner.scan(10.0)
+        for dev in devices:
+            print("Device address:", dev.addr)
+            print("  Device name:", dev.getValueText(9))
+            print("  RSSI:", dev.rssi)
+            # Estimate the distance based on the RSSI value
+            distance = ScanDelegate().estimateDistance(dev.rssi)
+            print("  Estimated distance:", distance, "meters")
+            print("  Advertising data:")
+            for (adtype, desc, value) in dev.getScanData():
+                print("    %s: %s" % (desc, value))
+                data = bytes(bytearray.fromhex(value))
+
+                ble_parser = BleParser()
+                sensor_msg, tracker_msg = ble_parser.parse_raw_data(data)
+                print(sensor_msg)
+
+            lat, lon = get_location(dev.addr)
+            print("  Latitude:", lat)
+            print("  Longitude:", lon)
+            service_data = get_service_data(dev)
+            if service_data:
+                print("  Service data:")
+                for key, value in service_data.items():
+                    print("    {}: {}".format(key, value))
+    except BTLEDisconnectError:
+        print("Device disconnected")
+    time.sleep(10)  # Delay for 10 seconds before scanning again
