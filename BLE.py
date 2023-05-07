99e43a4c5b74b35f0bd0a6e3ee5055b5938bfabe
a_rahman <a.rahman@nexum.srl>
2023-05-07 14:12:48 +0200

    arifur

--- BLE.py
+++ BLE.py
@@ -17,14 +17,25 @@ class ScanDelegate(DefaultDelegate):
         elif isNewData:
             print("Received new data from device:", dev.addr)
 
+     def estimateDistance(self, rssi):
+        # Calculate the distance based on the RSSI value using the log-distance path loss model
+        # The constants used in this formula are based on empirical measurements and can vary depending on the environment
+        txPower = -59 # The transmit power of the BLE device in dBm
+        n = 2.0 # The path loss exponent, which depends on the environment (e.g. free space, indoors, etc.)
+        return math.pow(10, (txPower - rssi) / (10 * n))
+       
+
 # Create a scanner object and set the delegate to handle incoming advertisements
 scanner = Scanner().withDelegate(ScanDelegate())
+while True:
+    # Scan for BLE devices for 10 seconds
+    devices = scanner.scan(10.0)
 
-# Scan for BLE devices for 10 seconds
-devices = scanner.scan(10.0)
+    # Print information about each discovered device
+    for dev in devices:
 
-# Print information about each discovered device
-for dev in devices:
-    print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
-    for (adtype, desc, value) in dev.getScanData():
-        print("  %s = %s" % (desc, value))
+        distance = ScanDelegate().estimateDistance(dev.rssi)
+        print("  Estimated distance:", distance, "meters")
+        print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
+        for (adtype, desc, value) in dev.getScanData():
+            print("  %s = %s" % (desc, value))
