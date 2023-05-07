c37f785b97077d80feb43662452705a853304149
a_rahman <a.rahman@nexum.srl>
2023-05-07 14:26:41 +0200

    arifur

--- BLE.py
+++ BLE.py
@@ -1,7 +1,8 @@
+import requests
+import json
 from bluepy.btle import Scanner, DefaultDelegate
-from geopy.geocoders import Nominatim
+import math 
 
-# Define a delegate class to handle incoming BLE advertisements
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
@@ -9,33 +10,50 @@ class ScanDelegate(DefaultDelegate):
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if isNewDev:
             print("Discovered device:", dev.addr)
-            # Get the location of the device based on its MAC address
-            geolocator = Nominatim(user_agent="my_app")
-            location = geolocator.geocode(dev.addr)
-            if location:
-                print("Device location:", location.latitude, location.longitude)
+            # Add the MAC address of the device to a list of nearby MAC addresses
+            nearby_macs.append(dev.addr)
         elif isNewData:
             print("Received new data from device:", dev.addr)
 
-     def estimateDistance(self, rssi):
+    def estimateDistance(self, rssi):
         # Calculate the distance based on the RSSI value using the log-distance path loss model
         # The constants used in this formula are based on empirical measurements and can vary depending on the environment
         txPower = -59 # The transmit power of the BLE device in dBm
         n = 2.0 # The path loss exponent, which depends on the environment (e.g. free space, indoors, etc.)
         return math.pow(10, (txPower - rssi) / (10 * n))
-       
 
 # Create a scanner object and set the delegate to handle incoming advertisements
 scanner = Scanner().withDelegate(ScanDelegate())
+
+# Initialize a list to store the MAC addresses of nearby devices
+nearby_macs = []
+
 while True:
     # Scan for BLE devices for 10 seconds
     devices = scanner.scan(10.0)
 
     # Print information about each discovered device
     for dev in devices:
-
         distance = ScanDelegate().estimateDistance(dev.rssi)
         print("  Estimated distance:", distance, "meters")
         print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
         for (adtype, desc, value) in dev.getScanData():
             print("  %s = %s" % (desc, value))
+
+    # Send a POST request to the Google Maps Geolocation API with the MAC addresses of nearby devices
+    if nearby_macs:
+        url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBqyGDMUcp4FZPGL6XICmX9ImxYzpIH99M"
+        data = {
+            "considerIp": "false",
+            "wifiAccessPoints": [{"macAddress": mac} for mac in nearby_macs]
+        }
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+        if response.status_code == 200:
+            location = response.json()["location"]
+            print("Estimated location:", location["lat"], location["lng"])
+        else:
+            print("Error:", response.status_code, response.text)
+
+    # Clear the list of nearby MAC addresses for the next scan
+    nearby_macs = []
