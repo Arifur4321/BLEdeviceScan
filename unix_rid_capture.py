ffd99c40b93b66842ec8eeded9c3ca0b2cfeff0f
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-08 15:26:02 +0200

    arifur

--- unix_rid_capture.py
+++ unix_rid_capture.py
@@ -1,35 +1,55 @@
-import requests
-import json
+from bluepy.btle import Scanner, DefaultDelegate
 from scapy.all import *
 
-# Set your Mapbox API key token
-mapbox_api_key = "sk.eyJ1IjoiYXJhaG1hbjMyMSIsImEiOiJjbGhlcHlvYWMwMTR3M25wZmJtZ3hwdWNqIn0.rU1nBTup7G13kx8nEGkE8g"
+class ScanDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
 
-# Initialize a list to store the MAC addresses of nearby access points
-nearby_macs = []
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev:
+            print("Discovered device:", dev.addr)
+        elif isNewData:
+            print("Received new data from device:", dev.addr)
 
-# Define a function to handle incoming WiFi packets
-def handle_packet(packet):
+def handle_wifi_packet(packet):
     if packet.haslayer(Dot11):
-        if packet.type == 0 and packet.subtype == 8:
-            # Add the MAC address of the access point to the list of nearby MAC addresses
-            if packet.addr2 not in nearby_macs:
-                nearby_macs.append(packet.addr2)
+        # Extract the MAC address of the access point and the client device
+        ap_mac = packet.addr2
+        client_mac = packet.addr1
 
-# Start capturing WiFi packets on the specified interface
-sniff(iface="wlan0mon", prn=handle_packet)
+        # Extract the signal strength of the packet
+        signal_strength = packet.dBm_AntSignal
 
-# Send a POST request to the Mapbox Geolocation API with the MAC addresses of nearby access points
-if nearby_macs:
-    #print("Nearby MAC addresses:", nearby_macs)
-    url = "https://api.mapbox.com/geocoding/v5/mapbox.places.json?access_token=" + mapbox_api_key
-    data = {
-        "wifi": [{"mac": mac} for mac in nearby_macs]
-    }
-    headers = {"Content-Type": "application/json"}
-    response = requests.post(url, data=json.dumps(data), headers=headers)
-    if response.status_code == 200:
-        location = response.json()["features"][0]["center"]
-        print("Estimated location:", location[1], location[0])
-    else:
-        print("Error:", response.status_code, response.text)
+        # Print out information about the packet
+        print("WiFi packet received:")
+        print("  Access point MAC:", ap_mac)
+        print("  Client device MAC:", client_mac)
+        print("  Signal strength:", signal_strength)
+
+def handle_bluetooth_packet(packet):
+    if packet.haslayer(BLE_ADvertising):
+        # Extract the MAC address of the Bluetooth device
+        mac_address = packet.addr
+
+        # Extract the signal strength of the packet
+        signal_strength = packet.dBm
+
+        # Print out information about the packet
+        print("Bluetooth packet received:")
+        print("  MAC address:", mac_address)
+        print("  Signal strength:", signal_strength)
+
+# Create a scanner object and set the delegate to handle incoming advertisements
+scanner = Scanner().withDelegate(ScanDelegate())
+
+# Start capturing packets on the WiFi and Bluetooth interfaces
+sniff(iface="wlan0", prn=handle_wifi_packet)
+sniff(iface="hci0", prn=handle_bluetooth_packet)
+
+while True:
+    # Scan for BLE devices for 10 seconds
+    devices = scanner.scan(10.0)
+
+    # Print information about each discovered device
+    for dev in devices:
+        print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
