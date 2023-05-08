310fe0557e2ca90d3fc59dc3003a7837a6d42ddf
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-08 12:53:36 +0200

    arifur

--- newparser.py
+++ newparser.py
@@ -1,15 +1,8 @@
-import asyncio
-import struct
-import time
-import json
-import msgpack
-import bleak
 import requests
-from bluepy.btle import Scanner, DefaultDelegate, BTLEDisconnectError
-import aioblescan as aiobs
-from bleparser import BleParser
+import json
+from bluepy.btle import Scanner, DefaultDelegate
+import math 
 
-# Define a custom delegate class to handle Bluetooth device events
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
@@ -17,32 +10,54 @@ class ScanDelegate(DefaultDelegate):
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if isNewDev:
             print("Discovered device:", dev.addr)
-            print("  Device name:", dev.getValueText(9))
-            print("  RSSI:", dev.rssi)
+            # Add the MAC address of the device to a list of nearby MAC addresses
+            nearby_macs.append(dev.addr)
         elif isNewData:
             print("Received new data from device:", dev.addr)
-            for (adtype, desc, value) in dev.getScanData():
-                print("    %s: %s" % (desc, value))
-                if desc == "Manufacturer":
-                    parser.parse_manufacturer_data(value)
 
-# Setup parser
-parser = BleParser(
-    discovery=False,
-    filter_duplicates=True
-)
+    def estimateDistance(self, rssi):
+        # Calculate the distance based on the RSSI value using the log-distance path loss model
+        # The constants used in this formula are based on empirical measurements and can vary depending on the environment
+        txPower = -59 # The transmit power of the BLE device in dBm
+        n = 2.0 # The path loss exponent, which depends on the environment (e.g. free space, indoors, etc.)
+        return math.pow(10, (txPower - rssi) / (10 * n))
 
-# Get everything connected
-loop = asyncio.get_event_loop()
+# Create a scanner object and set the delegate to handle incoming advertisements
+scanner = Scanner().withDelegate(ScanDelegate())
 
-#### Setup socket and controller
-socket = aiobs.create_bt_socket(0)
-fac = getattr(loop, "_create_connection_transport")(socket, aiobs.BLEScanRequester, None, None)
-conn, btctrl = loop.run_until_complete(fac)
+# Initialize a list to store the MAC addresses of nearby devices
+nearby_macs = []
 
-#### Attach callback
-btctrl.process = parser.parse_raw_data
-loop.run_until_complete(btctrl.send_scan_request(0))
+# Set your Mapbox API key token
+mapbox_api_key = "sk.eyJ1IjoiYXJhaG1hbjMyMSIsImEiOiJjbGhlcHlvYWMwMTR3M25wZmJtZ3hwdWNqIn0.rU1nBTup7G13kx8nEGkE8g"
 
-## Run forever
-loop.run_forever()
+while True:
+    # Scan for BLE devices for 10 seconds
+    devices = scanner.scan(10.0)
+
+    # Print information about each discovered device
+    for dev in devices:
+        distance = ScanDelegate().estimateDistance(dev.rssi)
+        print("  Estimated distance:", distance, "meters")
+        print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
+        for (adtype, desc, value) in dev.getScanData():
+            print("  %s = %s" % (desc, value))
+
+    # Send a POST request to the Mapbox Geolocation API with the MAC addresses of nearby devices
+    if nearby_macs:
+        print ("nearby mac :",nearby_macs)
+        url = "https://api.mapbox.com/geocoding/v5/mapbox.places.json?access_token=" + mapbox_api_key
+        data = {
+            "wifi": [{"mac": mac} for mac in nearby_macs]
+        }
+        print ("data :",data)
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+        if response.status_code == 200:
+            location = response.json()["features"][0]["center"]
+            print("Estimated location:", location[1], location[0])
+        else:
+            print("Error:", response.status_code, response.text)
+
+    # Clear the list of nearby MAC addresses for the next scan
+    nearby_macs = []
