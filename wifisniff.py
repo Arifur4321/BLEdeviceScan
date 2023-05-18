a1682c73499490370f3eae0a31d79f70fdb8e007
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-18 14:15:51 +0200

    arifur

--- wifisniff.py
+++ wifisniff.py
@@ -1,18 +1,17 @@
 import pyshark
 
 # Set up the Wi-Fi sniffer
-capture = pyshark.LiveCapture(interface='wlan1', display_filter='wlan.fc.type_subtype == 0x08')
+capture = pyshark.LiveCapture(interface='wlan1', display_filter='wlan.fc.type_subtype == 0x08 && wlan_mgt.tag_vendor_specific.oui == 0x001bc50f && wlan_mgt.tag.vendor_specific == 0x1e:16:fa:ff:0d:0d:12:00:5e:00')
 
 # Start the Wi-Fi sniffing loop
 for packet in capture.sniff_continuously():
 
-    # Check if the packet is an advertisement packet
-    if 'Beacon' in packet.wlan_mgt.subtype or 'Probe Response' in packet.wlan_mgt.subtype:
-        print('Advertisement packet:', packet)
+    # Check if the packet is a Wi-Fi RID packet
+    if 'RID' in packet.wlan_mgt.tag_vendor_specific:
+        print('Wi-Fi RID packet:', packet)
 
-        # Check if the packet has a WLAN management frame
-        if hasattr(packet, 'wlan_mgt'):
-            # Print out details of the advertisement packet
-            print('SSID:', packet.wlan_mgt.ssid)
-            print('BSSID:', packet.wlan.bssid)
-            print('Signal strength:', packet.radiotap.dbm_antsignal)
+        # Print out details of the Wi-Fi RID packet
+        print('RID:', packet.wlan_mgt.tag_vendor_specific.RID)
+        print('Version:', packet.wlan_mgt.tag_vendor_specific.Version)
+        print('Device ID:', packet.wlan_mgt.tag_vendor_specific.DeviceID)
+        print('Data:', packet.wlan_mgt.tag_vendor_specific.Data)
