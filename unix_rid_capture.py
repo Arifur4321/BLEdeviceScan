06e96944f98d746d2f9663f54e86132b515949d9
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-18 15:05:30 +0200

    arifur

--- unix_rid_capture.py
+++ unix_rid_capture.py
@@ -1,6 +1,11 @@
 from bluepy.btle import Scanner, DefaultDelegate
 from scapy.all import *
 
+import requests
+import json
+from bluepy.btle import Scanner, DefaultDelegate
+import math 
+
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
@@ -10,6 +15,14 @@ class ScanDelegate(DefaultDelegate):
             print("Discovered device:", dev.addr)
         elif isNewData:
             print("Received new data from device:", dev.addr)
+
+    def estimateDistance(self, rssi):
+        # Calculate the distance based on the RSSI value using the log-distance path loss model
+        # The constants used in this formula are based on empirical measurements and can vary depending on the environment
+        txPower = -59 # The transmit power of the BLE device in dBm
+        n = 2.0 # The path loss exponent, which depends on the environment (e.g. free space, indoors, etc.)
+        return math.pow(10, (txPower - rssi) / (10 * n))
+
 
 def handle_wifi_packet(packet):
     if packet.haslayer(Dot11):
@@ -42,9 +55,7 @@ def handle_bluetooth_packet(packet):
 # Create a scanner object and set the delegate to handle incoming advertisements
 scanner = Scanner().withDelegate(ScanDelegate())
 
-# Start capturing packets on the WiFi and Bluetooth interfaces
-sniff(iface="wlan0", prn=handle_wifi_packet)
-sniff(iface="hci0", prn=handle_bluetooth_packet)
+
 
 while True:
     # Scan for BLE devices for 10 seconds
@@ -53,5 +64,11 @@ while True:
     # Print information about each discovered device
     for dev in devices:
         print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
+        distance = ScanDelegate().estimateDistance(dev.rssi)
+        print("  Estimated distance:", distance, "meters")
+
+        # Start capturing packets on the WiFi and Bluetooth interfaces
+        sniff(iface="wlan1", prn=handle_wifi_packet)
+        sniff(iface="hci0", prn=handle_bluetooth_packet)
 
          
