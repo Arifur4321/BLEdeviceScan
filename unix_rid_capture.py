267b7be05e0527a970b7f740aa91fdb8be71043a
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-18 15:42:28 +0200

    arifur

--- unix_rid_capture.py
+++ unix_rid_capture.py
@@ -1,76 +1,24 @@
-from bluepy.btle import Scanner, DefaultDelegate
-from scapy.all import *
+from btlejack import ACLStream
+from scapy.layers.bluetooth import *
 
-import requests
-import json
-from bluepy.btle import Scanner, DefaultDelegate
-import math 
+# Create an ACL stream object to capture BLE packets
+stream = ACLStream()
 
-class ScanDelegate(DefaultDelegate):
-    def __init__(self):
-        DefaultDelegate.__init__(self)
+# Start capturing packets on the HCI interface
+stream.start_hci_capture()
 
-    def handleDiscovery(self, dev, isNewDev, isNewData):
-        if isNewDev:
-            print("Discovered device:", dev.addr)
-        elif isNewData:
-            print("Received new data from device:", dev.addr)
-
-    def estimateDistance(self, rssi):
-        # Calculate the distance based on the RSSI value using the log-distance path loss model
-        # The constants used in this formula are based on empirical measurements and can vary depending on the environment
-        txPower = -59 # The transmit power of the BLE device in dBm
-        n = 2.0 # The path loss exponent, which depends on the environment (e.g. free space, indoors, etc.)
-        return math.pow(10, (txPower - rssi) / (10 * n))
-
-
-def handle_wifi_packet(packet):
-    if packet.haslayer(Dot11):
-        # Extract the MAC address of the access point and the client device
-        ap_mac = packet.addr2
-        client_mac = packet.addr1
-
-        # Extract the signal strength of the packet
-        signal_strength = packet.dBm_AntSignal
-
-        # Print out information about the packet
-        print("WiFi packet received:")
-        print("  Access point MAC:", ap_mac)
-        print("  Client device MAC:", client_mac)
-        print("  Signal strength:", signal_strength)
-
-def handle_bluetooth_packet(packet):
-    if packet.haslayer(BLE_ADvertising):
-        # Extract the MAC address of the Bluetooth device
-        mac_address = packet.addr
-
-        # Extract the signal strength of the packet
-        signal_strength = packet.dBm
-
-        # Print out information about the packet
-        print("Bluetooth packet received:")
-        print("  MAC address:", mac_address)
-        print("  Signal strength:", signal_strength)
-
-# Create a scanner object and set the delegate to handle incoming advertisements
-scanner = Scanner().withDelegate(ScanDelegate())
-
-
-
+# Process incoming packets
 while True:
-    # Scan for BLE devices for 10 seconds
-    devices = scanner.scan(10.0)
+    # Get the next packet from the stream
+    packet = stream.get_packet()
 
-    # Print information about each discovered device
-    for dev in devices:
-        print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
-        for (adtype, desc, value) in dev.getScanData():
-            print("  %s = %s" % (desc, value))
-            distance = ScanDelegate().estimateDistance(dev.rssi)
-            print("  Estimated distance:", distance, "meters")
+    # Check if the packet is a BLE data packet
+    if packet.type == ACLDataPacket:
+        # Decode the packet using the scapy Bluetooth layer
+        decoded_packet = Bluetooth(packet.data)
 
-            # Start capturing packets on the WiFi and Bluetooth interfaces
-            sniff(iface="wlan1", prn=handle_wifi_packet)
-            sniff(iface="hci0", prn=handle_bluetooth_packet)
-
-         
+        # Print out information about the packet
+        print("BLE packet received:")
+        print("  Source address:", decoded_packet.src)
+        print("  Destination address:", decoded_packet.dst)
+        print("  Payload:", decoded_packet.payload)
