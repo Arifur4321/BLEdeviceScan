9ddee40e1d9f2c9dd715f7f4b5a0e0e43e7aadaf
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-18 14:27:44 +0200

    arifur

--- wifisniff.py
+++ wifisniff.py
@@ -1,63 +1,51 @@
-"""
-Wifi Sniffer
-"""
+#!/usr/bin/python
+#
+# A very simple python-pcapy example for monitor mode WiFi sniffing.
+#
+# Usage example:
+#   $ python pcapySniffer.py mon0
 
+import pcapy
+import sys
 import os
-from scapy.all import *
-
-if len(sys.argv) ==  2:
-	iface = str(sys.argv[1])
-else:
-	iface = "wlan0"
-
-os.system("ifconfig " + iface + " down") 
-os.system("iwconfig " + iface + " mode monitor")
-os.system("ifconfig " + iface + " up")
-
-def packet_info(pkt):
-    bssid = pkt[Dot11].addr3
-    p = pkt[Dot11Elt]
-    cap = pkt.sprintf("{Dot11Beacon:%Dot11Beacon.cap%}"
-                      "{Dot11ProbeResp:%Dot11ProbeResp.cap%}").split('+')
-    ssid, channel = None, None
-    crypto = set()
-    while isinstance(p, Dot11Elt):
-        if p.ID == 0:
-            ssid = p.info
-        elif p.ID == 3:
-            channel = ord(p.info)
-        elif p.ID == 48:
-            crypto.add("WPA2")
-        elif p.ID == 221 and p.info.startswith('\x00P\xf2\x01\x01\x00'):
-            crypto.add("WPA")
-        p = p.payload
-    if not crypto:
-        if 'privacy' in cap:
-            crypto.add("WEP")
-        else:
-            crypto.add("OPN")
-    print "    %r [%s], %s" % (ssid, bssid,' / '.join(crypto) )
+import datetime
 
 
+def main():
+    if len(sys.argv) != 2:
+        print "Available devices:"
+        print
+        devices = pcapy.findalldevs()
 
-def handle_packet(packet):
-	if not packet.haslayer(Dot11Beacon) and not packet.haslayer(Dot11ProbeReq) and not packet.haslayer(Dot11ProbeResp):
-		#regular packet
-		print "    " + packet.summary()
-		if packet.haslayer(Raw):
-			print "\n"
-			print hexdump(packet.load)
-			print "\n"	
-	elif packet.haslayer(Dot11ProbeReq) or packet.haslayer(Dot11ProbeResp) or packet.haslayer(Dot11AssoReq):
-		#http://stackoverflow.com/questions/21613091/how-to-use-scapy-to-determine-wireless-encryption-type
-		packet_info(packet)
-		
+        for device in devices:
+            print device
 
-print "[+] Sniffing on interface " + iface + ": "
+        print
+        print "Usage: ./%s deviceName", sys.argv[0]
+        exit()
 
-while True:
-	for channel in range(1, 14):
-		os.system("iwconfig " + iface + " channel " + str(channel))
-		print "[+] Sniffing on channel " + str(channel)
+    dev = sys.argv[1]
 
-		sniff(iface=iface, prn=handle_packet, count=10, timeout=3, store=0)
+    print "Trying to set monitor mode for device " + dev + "..."
+    os.system("ifconfig " + dev + " down")
+    os.system("iwconfig " + dev + " mode monitor")
+    os.system("ifconfig " + dev + " up")
+    print "Done. If you don't see any data, the monitor mode setup may have failed."
+
+    cap = pcapy.open_live(dev, 65536, True, 0)
+
+    print
+    print "Listening on %s: net=%s, mask=%s, linktype=%d" % (dev, cap.getnet(), cap.getmask(), cap.datalink())
+
+    (header, payload) = cap.next()
+    while header:
+        print ('%s: captured %d bytes, truncated to %d bytes'
+               % (datetime.datetime.now(), header.getlen(), header.getcaplen()))
+
+        # TODO: Implement python-impacket in order to decode the captured packet and show his information
+
+        (header, payload) = cap.next()
+
+
+if __name__ == "__main__":
+    main()
