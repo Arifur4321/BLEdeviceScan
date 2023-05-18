9cac528d9dbd8889503dc6a4626ba84604544aa6
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-18 13:58:25 +0200

    arifur

--- wifisniff.py
+++ wifisniff.py
@@ -9,7 +9,10 @@ for packet in capture.sniff_continuously():
     # Check if the packet is an advertisement packet
     if 'Beacon' in packet.wlan_mgt.subtype or 'Probe Response' in packet.wlan_mgt.subtype:
         print('Advertisement packet:', packet)
-        # Print out details of the advertisement packet
-        print('SSID:', packet.wlan_mgt.ssid)
-        print('BSSID:', packet.wlan.bssid)
-        print('Signal strength:', packet.radiotap.dbm_antsignal)
+
+        # Check if the packet has a WLAN management frame
+        if hasattr(packet, 'wlan_mgt'):
+            # Print out details of the advertisement packet
+            print('SSID:', packet.wlan_mgt.ssid)
+            print('BSSID:', packet.wlan.bssid)
+            print('Signal strength:', packet.radiotap.dbm_antsignal)
