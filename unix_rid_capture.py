52df6922d13a85e2cceaf69183fda0a75b801c26
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-18 15:55:40 +0200

    arifur

--- unix_rid_capture.py
+++ unix_rid_capture.py
@@ -1,4 +1,8 @@
 from bluepy.btle import Scanner, DefaultDelegate, Peripheral
+import requests
+import json
+from bluepy.btle import Scanner, DefaultDelegate
+import math 
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
@@ -10,6 +14,13 @@ class ScanDelegate(DefaultDelegate):
         elif isNewData:
             print("Received new data from device:", dev.addr)
 
+    def estimateDistance(self, rssi):
+        # Calculate the distance based on the RSSI value using the log-distance path loss model
+        # The constants used in this formula are based on empirical measurements and can vary depending on the environment
+        txPower = -59 # The transmit power of the BLE device in dBm
+        n = 2.0 # The path loss exponent, which depends on the environment (e.g. free space, indoors, etc.)
+        return math.pow(10, (txPower - rssi) / (10 * n))        
+
 # Create a scanner object and set the delegate to handle incoming advertisements
 scanner = Scanner().withDelegate(ScanDelegate())
 
@@ -20,12 +31,19 @@ while True:
     # Print information about each discovered device
     for dev in devices:
         print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
+        distance = ScanDelegate().estimateDistance(dev.rssi)
+        print("  Estimated distance:", distance, "meters")
+        print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
+        for (adtype, desc, value) in dev.getScanData():
+            print("  %s = %s" % (desc, value))
+
 
         # Connect to the device and retrieve its characteristics
         try:
+            print ("i enter here in try ")
             peripheral = Peripheral(dev.addr, "public")
             characteristics = peripheral.getCharacteristics()
-
+            print ("characteristics",characteristics)   
             # Read the value of each characteristic to capture packets
             for characteristic in characteristics:
                 value = characteristic.read()
