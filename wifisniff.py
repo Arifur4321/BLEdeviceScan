27d989ee1e330c9cfe0aae9ff2c7d10378e83d1b
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-18 13:52:58 +0200

    arifur

--- wifisniff.py
+++ wifisniff.py
@@ -1,36 +1,15 @@
 import pyshark
 
 # Set up the Wi-Fi sniffer
-#capture = pyshark.LiveCapture(interface='wlan1', display_filter='wlan.fc.type_subtype == 0x0d')
-#capture = pyshark.LiveCapture(interface='wlan1', display_filter='wlan.fc.type_subtype == 0x08 && wlan_mgt.tag_vendor_specific.oui == 0x001bc50f && wlan_mgt.tag.vendor_specific == 0x1e:16:fa:ff:0d:0d:12:00:5e:00')
-capture = pyshark.LiveCapture(interface='wlan1', display_filter='wlan.fc.type_subtype == 0x08 && wlan_mgt.tag_vendor_specific.oui == 0x001bc50f')
+capture = pyshark.LiveCapture(interface='wlan1', display_filter='wlan.fc.type_subtype == 0x08')
 
 # Start the Wi-Fi sniffing loop
 for packet in capture.sniff_continuously():
 
-    print('all Wi-Fi RID packets:', packet)
-    # Check if the packet is a Wi-Fi RID packet
-    if 'wlan_mgt.rsn_ie' in packet:
-        # Print the Wi-Fi RID details
-        print('Wi-Fi RID:', packet.wlan_mgt.rsn_ie)
-
-    # Check if the packet is an ASTM F3411 packet
-    if 'btatt' in packet and packet.btatt.handle_value == '0x0025':
-        # Print the ASTM F3411 details
-        print('ASTM F3411:', packet.btatt.value)
-
-    # Check if the packet is a Wi-Fi probe request packet
-    if 'wlan_mgt.tag.oui' in packet and packet.wlan_mgt.tag.oui == '00:50:f2':
-        # Get the MAC address of the nearby device
-        mac_address = packet.wlan.ta
-
-        # Get the latitude, longitude, and altitude of the nearby device
-        latitude = packet.wlan_mgt.tag.get_field('wlan_mgt.tag.custom_data').showname_value.split(',')[0]
-        longitude = packet.wlan_mgt.tag.get_field('wlan_mgt.tag.custom_data').showname_value.split(',')[1]
-        altitude = packet.wlan_mgt.tag.get_field('wlan_mgt.tag.custom_data').showname_value.split(',')[2]
-
-        # Print the nearby device details
-        print('MAC Address:', mac_address)
-        print('Latitude:', latitude)
-        print('Longitude:', longitude)
-        print('Altitude:', altitude)
+    # Check if the packet is an advertisement packet
+    if 'Beacon' in packet.wlan_mgt.subtype or 'Probe Response' in packet.wlan_mgt.subtype:
+        print('Advertisement packet:', packet)
+        # Print out details of the advertisement packet
+        print('SSID:', packet.wlan_mgt.ssid)
+        print('BSSID:', packet.wlan.bssid)
+        print('Signal strength:', packet.radiotap.dbm_antsignal)
