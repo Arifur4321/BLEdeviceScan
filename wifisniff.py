c1b6ced28678bb61944712d7c3ee3fb65fe3e800
Arifur4321 <arifur.rahman07@outlook.com>
2023-05-19 14:27:22 +0200

    arifur

--- wifisniff.py
+++ wifisniff.py
@@ -1,14 +1,11 @@
-import pyshark
+from scapy.all import *
 
-# Set up the Wi-Fi sniffer
-capture = pyshark.LiveCapture(interface='wlan1', display_filter='wlan.fc.type_subtype == 0x08')
+def handle_packet(packet):
+    if packet.haslayer(Dot11InformationElement) and packet[Dot11InformationElement].ID == 221:
+        # Check if the packet is a RID packet (ID = 221)
+        rid = packet[Dot11InformationElement].info
+        print("RID packet:", rid)
 
-# Start the Wi-Fi sniffing loop
-for packet in capture.sniff_continuously():
-
-    # Check if the packet has a 16-bit service data field
-    if hasattr(packet, 'wlan_mgt.tag_vendor_specific') and '16-bit Service Data' in packet.wlan_mgt.tag_vendor_specific:
-        print('Wi-Fi packet with 16-bit service data:', packet)
-
-        # Print out details of the 16-bit service data
-        print('Service data:', packet.wlan_mgt.tag_vendor_specific['16-bit Service Data'])
+# Start capturing packets on the specified interface
+interface = "wlan0"
+sniff(iface=interface, prn=handle_packet)
