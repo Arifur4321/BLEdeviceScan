a596b2a3953a7dd2b119aca201fa57a17099b004
Arifur4321 <arifur.rahman07@outlook.com>
2023-07-13 15:54:31 +0200

    arifur

--- mypractice2.py
+++ mypractice2.py
@@ -19,8 +19,18 @@ class ODIDScanDelegate(btle.DefaultDelegate):
                 if adtype == 22 and desc == "16b Service Data":
                     print("opendroneid:", dev.addr)
                     print("16b service data", value)
-                    service_data = binascii.unhexlify(value)
-                    decode_service_data(service_data)
+                   # parse_packet(value)
+		   
+
+                    if value.startswith("faff"):
+                        print("The string starts with 'faff'")
+                        service_data = binascii.unhexlify(value)
+                        decode_service_data(service_data)
+
+                    else:
+                        print("This not opendroneid data")
+
+                    
 def decode_latitude(bits):
 		# Convert the bits to a decimal value
 		latitude_decimal = 0
@@ -64,8 +74,15 @@ def decode_service_data(service_data):
 	direction = service_data[2] & 0b00000011
 	speed = service_data[3]
 	vert_speed = service_data[4]
-	ua_latitude = struct.unpack('!I', service_data[5:9])[0]
-	ua_longitude = struct.unpack('!I', service_data[9:13])[0]
+	latitude =  struct.unpack('!I',service_data[5:9])[0]
+	longitude = struct.unpack('!I',service_data[9:13])[0]
+	#latitude = int.from_bytes(service_data[5:9], byteorder='big', signed=True)
+	#longitude = int.from_bytes(service_data[9:13], byteorder='big', signed=True)
+	#latitude= service_data[5:9]
+	#longitude=service_data[9:13]       
+    # Convert latitude and longitude to decimal degrees with 4 decimal places
+	#latitude = latitude_int / 10**7
+	#longitude = longitude_int / 10**7
 	ua_pressure_altitude = struct.unpack('!H', service_data[13:15])[0]
 	ua_geodetic_altitude = struct.unpack('!H', service_data[15:17])[0]
 	ua_height_agl = struct.unpack('!H', service_data[17:19])[0]
@@ -82,25 +99,24 @@ def decode_service_data(service_data):
  	#latitude_bits = service_data[5:9]
 	#longitude_bits = service_data[9:13]
 	#latitude = decode_latitude(latitude_bits)
-	#longitude = decode_longitude(longitude_bits)
-
-	 
+	#longitude = decode_longitude(longitude_bi
 
 
 
 	# Print the decoded fields
-	print("Message Counter:", message_counter)
-	print("Message Type:", message_type)
-	print("Protocol Version:", protocol_version)
-	print("Operational Status:", operational_status)
-	print("Height Type:", height_type)
-	print("East/West Direction Segment:", east_west_direction)
-	print("Speed Multiplier:", speed_multiplier)
-	print("Direction:", direction)
+	print("Open Drone ID Advertisement:")
+	print(f"Message Counter: {message_counter}")
+	print("Open Drone ID - Location/Vector Message")
+	print(f"Message Type: Location/Vector ({message_type})")
+	print(f"Protocol Version: F3411-22 ({protocol_version})")
+	print(f"Operational Status: {'Airborne' if operational_status == 2 else 'Not Airborne'}")
+	print(f"Height Type: {'Above Takeoff' if height_type == 0 else 'Above Ground Level'}")
+	print(f"Direction: {direction}")
+   
 	print("Speed:", speed)
 	print("Vert Speed:", vert_speed)
-	print("UA Latitude:", ua_latitude)
-	print("UA Longitude:", ua_longitude)
+	print("UA Latitude:", latitude)
+	print("UA Longitude:", longitude)
 	print("UA Pressure Altitude:", ua_pressure_altitude)
 	print("UA Geodetic Altitude:", ua_geodetic_altitude)
 	print("UA Height AGL:", ua_height_agl)
@@ -112,6 +128,65 @@ def decode_service_data(service_data):
 	print("Reserved:", reserved)
 	print("Timestamp Accuracy:", timestamp_accuracy)
 
+
+
+
+
+def parse_packet(packet):
+    if len(packet) < 42:
+        print("Invalid packet length")
+        return
+
+    # Convert the hex packet string to bytes
+    packet_bytes = bytes.fromhex(packet)
+
+    # Parse the packet according to the Open Drone ID specification
+    message_counter = packet_bytes[0]
+    message_type = packet_bytes[1] >> 4
+    protocol_version = packet_bytes[1] & 0b00001111
+    operational_status = packet_bytes[2] >> 6
+    height_type = (packet_bytes[2] >> 4) & 0b00000011
+    direction = packet_bytes[2] & 0b00111111
+    speed = packet_bytes[3]
+    vert_speed = packet_bytes[4]
+   # latitude = int.from_bytes(packet_bytes[5:9], byteorder='big')
+   # longitude = int.from_bytes(packet_bytes[9:13], byteorder='big')
+    latitude = int.from_bytes(packet_bytes[5:9], byteorder='big', signed=True) / 10**7
+    longitude = int.from_bytes(packet_bytes[9:13], byteorder='big', signed=True) / 10**7 
+
+    pressure_altitude = int.from_bytes(packet_bytes[13:15], byteorder='big')
+    geodetic_altitude = int.from_bytes(packet_bytes[15:17], byteorder='big')
+    height_agl = int.from_bytes(packet_bytes[17:19], byteorder='big')
+    horizontal_accuracy = packet_bytes[19] >> 4
+    vertical_accuracy = packet_bytes[19] & 0b00001111
+    baro_accuracy = packet_bytes[20] >> 4
+    speed_accuracy = packet_bytes[20] & 0b00001111
+    timestamp = int.from_bytes(packet_bytes[21:23], byteorder='big')
+    timestamp_accuracy = packet_bytes[24] >> 4
+
+    # Print the parsed data
+    print("Open Drone ID Advertisement:")
+    print(f"Message Counter: {message_counter}")
+    print("Open Drone ID - Location/Vector Message")
+    print(f"Message Type: Location/Vector ({message_type})")
+    print(f"Protocol Version: F3411-22 ({protocol_version})")
+    print(f"Operational Status: {'Airborne' if operational_status == 2 else 'Not Airborne'}")
+    print(f"Height Type: {'Above Takeoff' if height_type == 0 else 'Above Ground Level'}")
+    print(f"Direction: {direction}")
+    print(f"Speed: {speed}")
+    print(f"Vert Speed: {vert_speed}")
+    print(f"UA Latitude: {latitude}")
+    print(f"UA Longitude: {longitude}")
+    print(f"UA Pressure Altitude: {pressure_altitude}")
+    print(f"UA Geodetic Altitude: {geodetic_altitude}")
+    print(f"UA Height AGL: {height_agl}")
+    print(f"Horizontal Accuracy: <{horizontal_accuracy} m")
+    print(f"Vertical Accuracy: <{vertical_accuracy} m")
+    print(f"Baro Accuracy: <{baro_accuracy} m")
+    print(f"Speed Accuracy: <{speed_accuracy} m/s")
+    print(f"Timestamp: {timestamp}")
+    print(f"Timestamp Accuracy: {timestamp_accuracy}")
+    print("Reserved:", packet_bytes[23:24].hex())
 
 # Create a scanner object and set the delegate
 scanner = btle.Scanner().withDelegate(ODIDScanDelegate())
@@ -130,5 +205,11 @@ while True:
             if adtype == 22 and desc == "16b Service Data":
                 print("opendroneid:", dev.addr)
                 print("16b service data", value)
-                service_data = binascii.unhexlify(value)
-                decode_service_data(service_data)
+                #parse_packet(value) 
+                if value.startswith("faff"):
+                    print("The string starts with 'faff'")
+                    service_data = binascii.unhexlify(value)
+                    decode_service_data(service_data)
+
+                else:
+                    print("This not opendroneid data")
