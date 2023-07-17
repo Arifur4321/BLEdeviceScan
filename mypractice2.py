f4a8e48b9ec2aded9a79961bf6da098326af3396
Arifur4321 <arifur.rahman07@outlook.com>
2023-07-17 12:28:28 +0200

    arifur

--- mypractice2.py
+++ mypractice2.py
@@ -26,6 +26,9 @@ class ODIDScanDelegate(btle.DefaultDelegate):
                         print("The string starts with 'faff'")
                         service_data = binascii.unhexlify(value)
                         decode_service_data(service_data)
+                        print("Device RSSI :", dev.rssi)
+                        distance = estimateDistance(dev.rssi)
+                        print("  Estimated distance:", distance, "meters")
 
                     else:
                         print("This not opendroneid data")
@@ -57,7 +60,12 @@ def decode_longitude(bits):
 		longitude_minutes = longitude_decimal % 10**7 / 60
 
 		return f"{longitude_degrees:.6f}, {longitude_minutes:.6f}"  
-
+def estimateDistance(self, rssi):
+        # Calculate the distance based on the RSSI value using the log-distance path loss model
+        # The constants used in this formula are based on empirical measurements and can vary depending on the environment
+        txPower = -59 # The transmit power of the BLE device in dBm
+        n = 2.0 # The path loss exponent, which depends on the environment (e.g. free space, indoors, etc.)
+        return math.pow(10, (txPower - rssi) / (10 * n))   
 
 def decode_service_data(service_data):
     # Example decoding logic
